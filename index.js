b8a8de7e5bd656709d5c5de8f5263599e259f6bb
falukki351-netizen <falukki351@gmail.com>
2026-06-23 08:13:48 +0200

    Fix syntax errors in index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const { Client, GatewayIntentBits, Events } = require("discord.js");
+ient, GatewayIntentBits, Events } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 client.once(Events.ClientReady, c => console.log(`✅ Online: ${c.user.tag}`));
 client.on(Events.MessageCreate, async msg => {
-  if (msg.author.bot) return;
-  if (msg.content === "!ping") await msg.reply("Pong! 🏓");
-  if (msg.content === "!help") await msg.reply("Befehle: !ping !help !info");
-  if (msg.content === "!info") await msg.reply(`Server: ${client.guilds.cache.size} | Uptime: ${Math.floor(process.uptime()/60)} Min`);
-});
-client.login(process.env.DISCORD_TOKEN);
+  if (msg.author.bot) return
