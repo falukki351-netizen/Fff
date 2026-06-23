72a05dd0f543c492f38344902c14d99f627d6dd4
falukki351-netizen <falukki351@gmail.com>
2026-06-23 08:19:24 +0200

    Refactor Discord bot and add HTTP server
    Refactor Discord bot initialization and add HTTP server.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-ient, GatewayIntentBits, Events } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-client.once(Events.ClientReady, c => console.log(`✅ Online: ${c.user.tag}`));
-client.on(Events.MessageCreate, async msg => {
-  if (msg.author.bot) return
+var discord = require("discord.js");
+var http = require("http");
+var client = new discord.Client({
+  intents: [
+    discord.GatewayIntentBits.Guilds,
+    discord.GatewayIntentBits.GuildMessages
+  ]
+});
+client.once("ready", function(c) {
+  console.log("Bot online: " + c.user.tag);
+});
+client.on("error", function(err) {
+  console.error("Fehler:", err);
+});
+client.login(process.env.DISCORD_TOKEN);
+var port = process.env.PORT || 3000;
+http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end("OK");
+}).listen(port);
